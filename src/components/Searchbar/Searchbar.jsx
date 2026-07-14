f3b2db884ef36509b9df029bb592f3b2e44362d4
sxndev <sxnsilva@gmail.com>
2026-07-14 01:56:42 -0300

    barra de pesquisar pokemon criada mas sem função

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  width: 80%;
+  height: 40px;
+  background: white;
+  background-color: var(--soft-bg-color);
+  border: none;
+  outline: none;
+  padding: 10px 25px;
+  border-radius: 10px;
+  z-index: 10;
+  cursor: pointer;
+  color: white;
+  font-size: 20px;
+
+  &::placeholder {
+    color: white;
+  }
+`;
+
+const StyledClearBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  width: 25px;
+  height: 25px;
+  padding: 20px;
+  font-size: 20px;
+  border-radius: 100%;
+  font-family: var(--font-3);
+  background-color: var(--accent-color);
+
+  transition: transform 0.5s ease;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
+
+const StyledDiv = styled.div`
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Searchbar = () => {
+  const [search, setSearch] = useState("");
+  
+  return (
+    <StyledDiv>
+      <StyledInput
+        id="input-search"
+        type="text"
+        placeholder="Buscar um pokemon"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <StyledClearBtn id="clear-btn" onClick={() => setSearch("")}>
+        X
+      </StyledClearBtn>
+    </StyledDiv>
+  );
+};
--- src/pages/PokemonsSection/PokemonsSection.jsx
+++ src/pages/PokemonsSection/PokemonsSection.jsx
@@ -1,11 +1,24 @@
 import { PokeList } from "../../components/PokeList/PokeList";
 import { Header } from "../../components/Header/Header";
+import { Searchbar } from "../../components/Searchbar/Searchbar";
+import styled from "styled-components";
 
+
+const StyledSection = styled.section`
+    padding-top:20vh;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    background-color:var(--bg-color);
+`
 export const PokemonSection = () => {
     return (
         <>
         <Header/>
+        <StyledSection>
+        <Searchbar/>
         <PokeList/>
+        </StyledSection>
         </>
     )
 }
