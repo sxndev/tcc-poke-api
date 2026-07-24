e184319f6ff168951cd4d456f7f7e726351c4cb5
Nicolas Silva <sxnsilva@gmail.com>
2026-07-24 17:14:20 -0300

    rota da seção de pokemons removida e rota sobre o projeto adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import "../src/css/vars.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/home";
 import { GlobalStyle } from "./css/GlobalStyle";  
-import { PokeDetails } from "./pages/PokemonDetails/PokemonDetails";
-import { PokemonSection } from "./pages/PokemonsSection/PokemonsSection";
-      
+import { About } from './pages/About/About.jsx'
+
 function App() {
   return (
     <>
     <GlobalStyle/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pokemons" element={<PokemonSection/>} />
-        <Route path="/pokemons/:pokedata" element={<PokeDetails/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </>
   );
