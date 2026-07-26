9a3ca2c5746d978aba82d8f0c10c76f5737a3b9b
Nicolas Silva <sxnsilva@gmail.com>
2026-07-26 02:29:38 -0300

    parâmetro da rota corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home.jsx";
 import { GlobalStyle } from "./css/GlobalStyle";  
 import { About } from './pages/About/About.jsx'
+import { PokeDetails } from "./pages/PokemonDetails/PokemonDetails.jsx";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
+        <Route path="/pokemon/:pokeData" element={<PokeDetails/>} />
+
       </Routes>
     </>
   );
